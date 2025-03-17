dd708faade12d4ab0699d9ae2a8e2699c3a8e7a0
Shoaib Solanki <shoaibsolanki73@gmail.com>
2025-03-17 12:49:46 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import CategoryBar from './components/CategoryBar';
-import MainBanner from './components/MainBanner';
-import ProductSection from './components/ProductSection';
 import './index.css';
+import Home from './page/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [currentSlide, setCurrentSlide] = useState(0);
 
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <CategoryBar />
-      <main className="max-w-screen-2xl mx-auto px-2 sm:px-4">
-        <MainBanner currentSlide={currentSlide} setCurrentSlide={setCurrentSlide} />
-        <ProductSection />
-      </main>
-    </div>
-  );
+
+  function App() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    );
+  
 }
 
 export default App;
