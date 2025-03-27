167443878469435e59ee392c3b60b93f8beeb929
Shoaib Solanki <shoaibsolanki73@gmail.com>
2025-03-27 13:36:02 +0530

    Rp Work start

---
+++ src/adminpage/Rpbonusemange.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomDataTable from '../admincomponents/Microcomponents/DataTable'
+
+const Rpbonusemange = () => {
+  return (
+    <div>
+        <CustomDataTable/>
+    </div>
+  )
+}
+
+export default Rpbonusemange
