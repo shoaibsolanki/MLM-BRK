3bbbc6095b5a5eacfd698b83d319c6242f16b753
Shoaib Solanki <shoaibsolanki73@gmail.com>
2025-04-02 14:38:32 +0530

    get sub cateory done

--- src/adminpage/SubCategoryPage.jsx
+++ src/adminpage/SubCategoryPage.jsx
@@ -1,40 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CustomDataTable from '../admincomponents/Microcomponents/DataTable'
 import { Edit } from 'lucide-react';
-
+import DataService from '../services/requestApi'
+import SubCategoryModal from '../admincomponents/Modals/SubCategorymodal';
 const SubCategoryPage = () => {
-    const handleEdit = (id) => {
+    
+    const { storeId, saasId } = JSON.parse(localStorage.getItem("user_data"));
+    const [data, setData] = useState([])
+    const [selectedSubCat, setSelectedsubcat] = useState('')
+    const [open, setOpen] = useState(false)
+    const fetchSubCategories = async () => {
+        try {
+            const response = await DataService.GetSubCategory(saasId, storeId)
+            setData(response.data.data)
+            console.log(response); // Handle the fetched data as needed
+        } catch (error) {
+            console.error('Error fetching subcategories:', error);
+        }
+    };
+   
+    useEffect(() => {
+      fetchSubCategories()
+    }, [])
+
+    const handleEdit = (row) => {
         // Handle edit action here
-        console.log(`Edit category with id: ${id}`);
+        setSelectedsubcat(row)
+        setOpen(true)
+       
     };
 
     const columns = [
-        { name: 'Category Name', selector: row => row.category_name, sortable: true },
-        { name: 'Sub Category Name', selector: row => row.sub_category, sortable: true },
+        { name: 'Category Name', selector: row => row.category_name || 'N/A', sortable: true },
         { 
             name: 'Image', 
-            selector: row => <img src={row.userId} alt="category" style={{ width: '50px', height: '50px' }} />, 
+            selector: row => (
+                <img 
+                    src={row.image_path} 
+                    alt="category" 
+                    style={{ width: '50px', height: '50px' }} 
+                />
+            ), 
             sortable: true 
         },
-        { name: 'Creation Date', selector: row => row.created_date, sortable: true },
         { 
             name: 'Action', 
             selector: row => (
-                <Edit className='cursor-pointer'/>
+                <Edit className='cursor-pointer' onClick={()=>handleEdit(row)}/>
             ), 
             sortable: true 
         },
     ];
 
-    const data = [
-        { id: 1, category_name: 'Electronics', sub_category:"Charger", userId: 'https://via.placeholder.com/50', created_date: '2023-01-01' },
-        { id: 2, category_name: 'Books', sub_category:"Book", userId: 'https://via.placeholder.com/50', created_date: '2023-02-01' },
-        { id: 3, category_name: 'Clothing', sub_category:"Shirt", userId: 'https://via.placeholder.com/50', created_date: '2023-03-01' },
-        { id: 4, category_name: 'Home Appliances', sub_category:"Mixture Machine ", userId: 'https://via.placeholder.com/50', created_date: '2023-04-01' },
-    ];
+    
   return (
     <div>
           <CustomDataTable columns={columns} data={data} title="Mange Sub Category"/>
+          <SubCategoryModal open={open} handleClose={()=>setOpen(false)} defaultValues={selectedSubCat}/>
     </div>
   )
 }
