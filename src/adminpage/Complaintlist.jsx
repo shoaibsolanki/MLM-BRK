8105172946f44c8792f0ce5e931effe2060df189
Shoaib Solanki <shoaibsolanki73@gmail.com>
2025-04-03 12:51:13 +0530

    Complain list

--- src/adminpage/Complaintlist.jsx
+++ src/adminpage/Complaintlist.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomDataTable from '../admincomponents/Microcomponents/DataTable'
-
+import DataService from '../services/requestApi'
+import { useEffect } from 'react';
+import { Button } from '@mui/material';
 const Complaintlist = () => {
+
+  const [data, setData] = useState([])
+  const {  saasId } = JSON.parse(localStorage.getItem("user_data"));
+  const GetComplain = async ()=>{
+    try {
+      const response = await DataService.GetComplain(saasId)
+      if(response.data.status){
+        setData(response.data.data)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    GetComplain();
+  }, []);
+
+
     const columns = [
         { name: 'Name', selector: row => row.name, sortable: true },
         { name: 'Mobile', selector: row => row.mobile, sortable: true },
@@ -11,13 +31,23 @@ const Complaintlist = () => {
         { name: 'Date', selector: row => row.date, sortable: true },
     ];
 
-    const data = [
-        { id: 1, name: 'John Doe', mobile: '1234567890', email: 'john@example.com', subject: 'Issue 1', complaint: 'Complaint details 1', date: '2023-01-01' },
-        { id: 2, name: 'Jane Smith', mobile: '0987654321', email: 'jane@example.com', subject: 'Issue 2', complaint: 'Complaint details 2', date: '2023-01-02' },
-        // Add more data as needed
-    ];
+    // const data = [
+    //     { id: 1, name: 'John Doe', mobile: '1234567890', email: 'john@example.com', subject: 'Issue 1', complaint: 'Complaint details 1', date: '2023-01-01' },
+    //     { id: 2, name: 'Jane Smith', mobile: '0987654321', email: 'jane@example.com', subject: 'Issue 2', complaint: 'Complaint details 2', date: '2023-01-02' },
+    //     // Add more data as needed
+    // ];
   return (
     <div>
+      {/* <div className='flex justify-between items-center bg-white rounded p-2'>
+                              <h1 className='text-xl'>Complaint Management</h1>
+                              <Button
+                                 variant='contained'
+                                 color='primary'
+                                  // onClick={()=>setAddmodal(true)}
+                              >
+                                 Add Complaint
+                              </Button>
+                          </div> */}
         <CustomDataTable columns={columns} data={data} title={"Complaint Management"}/>
     </div>
   )
