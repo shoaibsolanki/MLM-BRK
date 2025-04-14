6663c8b471824362bceeba9b4bf2c1cd0f4ef93c
Shoaib Solanki <shoaibsolanki73@gmail.com>
2025-04-14 15:23:15 +0530

    Kyc list

---
+++ src/adminpage/Kyclist.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import CustomDataTable from '../admincomponents/Microcomponents/DataTable'
+import DataService from '../services/requestApi'
+import { Edit, Eye } from 'lucide-react';
+const Kyclist = () => {
+    const {  saasId } = JSON.parse(localStorage.getItem("user_data"));
+    const [data, setData] = useState([])
+    const GetKycData= async()=>{
+        try {
+            const response = await DataService.GetListkyc(saasId)
+            if (response.data.status) {
+                setData(response?.data.data || [])
+            }
+        } catch (error) {
+            console.log(error )
+        }
+    }
+
+    useEffect(() => {
+        GetKycData()
+    }, [])
+    
+   const column = [
+  
+    {
+        name: 'Name',
+        selector: row => row.userName,
+        sortable: true,
+    },
+    {
+        name: 'Bank Name',
+        selector: row => row.bankName,
+        sortable: true,
+    },
+    {
+        name: 'Account Number',
+        selector: row => row.accountNo,
+        sortable: true,
+    },
+    {
+        name: 'status',
+        selector: row =>( 
+            <>
+            <div className={`${row.status =='Pending' ? 'bg-red-700': 'bg-green-600'} text-white p-2 rounded`}>{row.status}</div>
+            </>
+        ),
+        sortable: true,
+    },
+    {
+        name: 'Date Of Apply',
+        selector: row => row.applyDate,
+        sortable: true,
+    },
+   
+    {
+        name: 'Action',
+        cell: row => (
+            <div className="flex gap-2">
+              <Eye className='cursor-pointer'/>
+            </div>
+        ),
+    },
+];
+
+
+
+
+  return (
+    <div>
+        <CustomDataTable  data={data} columns={column} title={"KYC Management"} />
+    </div>
+  )
+}
+
+export default Kyclist
