da88adaa8c08a409b70ca96924b98437b384bf42
Shoaib Solanki <shoaibsolanki73@gmail.com>
2025-05-08 18:11:30 +0530

    Protected Router

--- src/ProtectedRouteForAdmin.jsx
+++ src/ProtectedRouteForAdmin.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
+import { useAuth } from './contexts/AuthConext'
+import NotPermission from './page/NotPermision'
 
 const ProtectedRouteForAdmin = (props) => {
+    const { Component , index, page} = props
     const token = localStorage.getItem("token")
     const userData = JSON.parse(localStorage.getItem("user_data"))
-    const { Component } = props
+    const {userType} = userData || {}
+    const { permission, isLoding, role } = useAuth();
 
+     
+    const checkPermission = (index, page) => {
+       console.log(index , page )
+        return (
+          userType === "ADMIN" ||
+          (( userType == "subadmin" ) &&
+          permission[index]?.name === page &&
+          permission[index]?.status === true )
+        );
+      };
+
+      if (isLoding && !userData || permission?.length == 0) {
+        return <div>Loading...</div>; // Optional: Add a spinner or loader
+      }
+    
+    //   if (!userData) {
+    //     return <Navigate to="/admin/login" />;
+    //   }
+    //   if (userType != role) {
+    //     // localStorage.clear()
+    //     return <Navigate to="/admin/login" />;
+    //   }
     return (
         <>
-            {token && userData?.userType === 'ADMIN' ? (
+            {checkPermission(index , page)? (
                 <Component />
             ) : (
-                <Navigate to="/admin/login" />
+                <NotPermission/>
             )}
         </>
     )
